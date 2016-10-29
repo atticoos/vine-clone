fa4d3f3c6ac4a953e4a2ba43a78534d5c62edbb6
Atticus White <contact@atticuswhite.com>
2016-10-29 04:02:35 -0400

    paginated scrollview

--- src/components/videoPost/index.js
+++ src/components/videoPost/index.js
@@ -15,10 +15,16 @@ class VideoPost extends React.Component {
   render () {
     var {
       video,
-      style
+      style,
+      playing,
+      paused,
+      onPlay,
+      onPause,
+      onLoop,
+      ...props
     } = this.props;
     return (
-      <View style={[styles.container, style]}>
+      <View style={[styles.container, style]} {...props}>
         <View style={styles.padding}>
           <Header
             user={video.user}
@@ -26,20 +32,20 @@ class VideoPost extends React.Component {
             loops={video.loops.count}
           />
         </View>
-        {!this.props.playing &&
+        {!playing &&
           <Thumbnail
             style={styles.thumbnail}
             url={video.thumbnailUrl}
             onPress={this.props.onPlay}
           />
         }
-        {this.props.playing &&
+        {playing &&
           <Video
             style={styles.thumbnail}
             url={video.videoUrl}
-            paused={this.props.paused}
-            onPress={this.props.paused ? this.props.onPlay : this.props.onPause}
-            onEnd={this.props.onLoop}
+            paused={paused}
+            onPress={paused ? onPlay : onPause}
+            onEnd={onLoop}
           />
         }
         <View style={styles.padding}>
